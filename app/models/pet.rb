cced77b41ee8eed0a6ac033bef9a4fb3911e9c22
Andre Torres <andrerndc@gmail.com>
2020-09-29 13:59:21 -0300

    Listagem, edit e update  de Pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,6 @@
 class Pet <ApplicationRecord
+    has_one_attached:avatar
     has_one :adoption
-    validates :name, :pet_type, presence: true
+    validates :name, :pet_type, :avatar, presence: true
     enum pet_type: { dog: 'dog', cat: 'cat', other: 'other' } 
 end
